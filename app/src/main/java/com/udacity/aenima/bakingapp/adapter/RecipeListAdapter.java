f04cddf29d0216e3684caa4e7380f20eed80c00e
xaenimax <xaenimax@users.noreply.github.com>
2018-03-25 16:31:47 +0200

    data binding on viewholder

--- app/src/main/java/com/udacity/aenima/bakingapp/adapter/RecipeListAdapter.java
+++ app/src/main/java/com/udacity/aenima/bakingapp/adapter/RecipeListAdapter.java
@@ -1,8 +1,18 @@
 package com.udacity.aenima.bakingapp.adapter;
 
+import android.databinding.DataBindingUtil;
+import android.provider.ContactsContract;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.udacity.aenima.bakingapp.BR;
+import com.udacity.aenima.bakingapp.R;
+import com.udacity.aenima.bakingapp.data.Recipe;
+import com.udacity.aenima.bakingapp.databinding.RecipeItemBinding;
+
+import java.util.List;
 
 import butterknife.ButterKnife;
 
@@ -12,26 +22,41 @@ import butterknife.ButterKnife;
 
 public class RecipeListAdapter extends RecyclerView.Adapter<RecipeListAdapter.RecipeViewHolder> {
 
+    private List<Recipe> mRecipeList;
+
+    public RecipeListAdapter(List<Recipe> recipeList){
+        mRecipeList = recipeList;
+    }
+
     @Override
     public RecipeViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater layoutInflater =  LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.recipe_item, parent, false);
+        RecipeItemBinding binding = DataBindingUtil.inflate(layoutInflater, R.layout.recipe_item, parent, false);
+        return new RecipeViewHolder(binding);
     }
 
     @Override
     public void onBindViewHolder(RecipeViewHolder holder, int position) {
+        Recipe recipe = mRecipeList.get(position);
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mRecipeList.size();
     }
 
     class RecipeViewHolder extends RecyclerView.ViewHolder{
+        private RecipeItemBinding recipeItemBinding;
 
-        public RecipeViewHolder(View itemView) {
-            super(itemView);
-            ButterKnife.bind(this.itemView);
+        public RecipeViewHolder(RecipeItemBinding binding) {
+            super(binding.getRoot());
+            recipeItemBinding = binding;
+        }
+        public void bind(Recipe recipe) {
+            recipeItemBinding.setVariable(BR.recipe, recipe);
+            recipeItemBinding.executePendingBindings();
         }
     }
 }
