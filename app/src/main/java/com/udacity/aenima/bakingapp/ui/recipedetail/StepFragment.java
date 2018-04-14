6083b91fbaa8c3f431c2933f1975241fa95bf30a
xaenimax <xaenimax@users.noreply.github.com>
2018-04-14 13:45:27 +0200

    little changes

--- app/src/main/java/com/udacity/aenima/bakingapp/ui/recipedetail/StepFragment.java
+++ app/src/main/java/com/udacity/aenima/bakingapp/ui/recipedetail/StepFragment.java
@@ -119,7 +119,7 @@ public class StepFragment extends Fragment {
             mListener = (OnStepSelectedListener) context;
         } else {
             throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+                    + " must implement OnStepSelectedListener");
         }
 
     }
--- app/src/main/java/com/udacity/aenima/bakingapp/ui/recipedetail/VideoFragment.java
+++ app/src/main/java/com/udacity/aenima/bakingapp/ui/recipedetail/VideoFragment.java
@@ -1,12 +1,14 @@
 package com.udacity.aenima.bakingapp.ui.recipedetail;
 
 
+import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -139,5 +141,11 @@ public class VideoFragment extends Fragment {
             mSimpleExoPlayer.release();
             mSimpleExoPlayer = null;
         }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        Log.d("VIDEO FRAGMENT", "activity attached");
+        super.onAttach(context);
     }
 }
