159473bbb7bccfbfa7c2e507d926aec403a468f1
Marina <marina@users.noreply.github.com>
2018-04-24 21:28:59 +0200

    widget and land-layout

--- app/src/main/java/com/udacity/aenima/bakingapp/BakingAppWidgetProvider.java
+++ app/src/main/java/com/udacity/aenima/bakingapp/BakingAppWidgetProvider.java
@@ -1,9 +1,13 @@
 package com.udacity.aenima.bakingapp;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
+
+import com.udacity.aenima.bakingapp.ui.Recipes.MainActivity;
 
 /**
  * Implementation of App Widget functionality.
@@ -17,7 +21,9 @@ public class BakingAppWidgetProvider extends AppWidgetProvider {
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.baking_app_widget_provider);
         views.setTextViewText(R.id.appwidget_text, widgetText);
-
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
