20b03e25a370665d8aabeece3b52efef95e907bd
Marina <marina@users.noreply.github.com>
2018-04-29 15:38:43 +0200

    setting up espresso tests

--- app/src/androidTest/java/com/udacity/aenima/bakingapp/DetailActivityBasicTest.java
+++ app/src/androidTest/java/com/udacity/aenima/bakingapp/DetailActivityBasicTest.java
@@ -1,14 +1,25 @@
 package com.udacity.aenima.bakingapp;
 
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.udacity.aenima.bakingapp.ui.recipedetail.DetailActivity;
+import com.udacity.aenima.bakingapp.ui.recipedetail.step.StepActivity;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.Intents.intending;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.isInternal;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import static org.hamcrest.CoreMatchers.not;
+
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,9 +29,40 @@ public class DetailActivityBasicTest {
     @Rule
     public ActivityTestRule<DetailActivity> detailActivityActivityTestRule = new ActivityTestRule<>(DetailActivity.class);
 
-    @Test
-    public void onSelectRecipeAsFavourite_WriteInSharedPreferences(){
-        onView(withId(R.id.add_fav_fb)).perform(click())
-
+    /*
+   @Before
+    public void Stub_Intent(){
+       intending(not(isInternal())).respondWith(new Instrumentation.ActivityResult(Activity.RESULT_OK,null));
     }
+    */
+    @Test
+    public void check_ingredients(){
+        onView(withId(R.id.step_list_rv)).perform(RecyclerViewActions.scrollToPosition(0)).perform(click());
+        intended(hasComponent(StepActivity.class.getName()));
+
+        //intended(allOf(hasExtra(Intent.EXTRA_TEXT,"1")));
+        //onView(withId(R.id.ingrediets_recycler)).perform(RecyclerViewActions.scrollToHolder(withIngredientsRecyclerView("salt")));
+    }
+/*
+
+    public static org.hamcrest.Matcher<RecyclerView.ViewHolder > withIngredientsRecyclerView(final String text) {
+        return new BoundedMatcher<RecyclerView.ViewHolder, RecyclerIngredientsAdapter.IngredientsViewHolder>(RecyclerIngredientsAdapter.IngredientsViewHolder.class) {
+
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("No se encontró "+text);
+            }
+
+            @Override
+            protected boolean matchesSafely(RecyclerIngredientsAdapter.IngredientsViewHolder item) {
+                TextView textView = item.itemView.findViewById(R.id.ingredient_name);
+                if (textView==null){
+                    return false;
+                }
+                return textView.getText().toString().contains(text);
+            }
+        };
+    }
+*/
 }
--- app/src/androidTest/java/com/udacity/aenima/bakingapp/MainActivityBasicTest.java
+++ app/src/androidTest/java/com/udacity/aenima/bakingapp/MainActivityBasicTest.java
@@ -3,9 +3,12 @@ package com.udacity.aenima.bakingapp;
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.IdlingRegistry;
 import android.support.test.espresso.IdlingResource;
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.udacity.aenima.bakingapp.ui.recipedetail.DetailActivity;
+import com.udacity.aenima.bakingapp.ui.recipedetail.step.StepActivity;
 import com.udacity.aenima.bakingapp.ui.recipes.MainActivity;
 
 import org.junit.After;
@@ -15,33 +18,46 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityBasicTest {
     @Rule
     public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
-    private IdlingResource mIdlingResource;
-
+    //private IdlingResource mIdlingResource;
+    /*
     @Before
     public void registerIdlingResource(){
+
         mIdlingResource = mainActivityActivityTestRule.getActivity().getIdlingResource();
         //Espresso.registerIdlingResources() deprecated
         IdlingRegistry.getInstance().register(mIdlingResource);
+
     }
+    */
+
     @Test
     public void onRecipeClick_OpenDetailActivity(){
     //Find the view
     //Action on the view
-        //onView((withId(R.id.add_fav_fb)))
+        onView(withText("Baking App")).check(matches(isDisplayed()));
+       // onView(withId(R.id.recipe_list_rv)).perform(RecyclerViewActions.scrollToPosition(0)).perform(click());
+        //intended(hasComponent(DetailActivity.class.getName()));
     //Check view action
     }
-
+/*
     @After
     public void unregisterIdlingResource(){
         if(mIdlingResource != null){
             IdlingRegistry.getInstance().unregister(mIdlingResource);
         }
     }
+    */
 }
