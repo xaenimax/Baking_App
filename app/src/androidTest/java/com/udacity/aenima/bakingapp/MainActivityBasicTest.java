e0ef38f59c9c988423f11b0472a13f426f9c4f24
Marina <marina@users.noreply.github.com>
2018-04-29 12:41:09 +0200

    setting up espresso tests

--- app/src/androidTest/java/com/udacity/aenima/bakingapp/MainActivityBasicTest.java
+++ app/src/androidTest/java/com/udacity/aenima/bakingapp/MainActivityBasicTest.java
@@ -1,10 +1,15 @@
 package com.udacity.aenima.bakingapp;
 
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.IdlingRegistry;
+import android.support.test.espresso.IdlingResource;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.udacity.aenima.bakingapp.ui.recipes.MainActivity;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,11 +22,26 @@ public class MainActivityBasicTest {
     @Rule
     public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
+    private IdlingResource mIdlingResource;
+
+    @Before
+    public void registerIdlingResource(){
+        mIdlingResource = mainActivityActivityTestRule.getActivity().getIdlingResource();
+        //Espresso.registerIdlingResources() deprecated
+        IdlingRegistry.getInstance().register(mIdlingResource);
+    }
     @Test
     public void onRecipeClick_OpenDetailActivity(){
     //Find the view
     //Action on the view
-        onView((withId(R.id.add_fav_fb)))
+        //onView((withId(R.id.add_fav_fb)))
     //Check view action
+    }
+
+    @After
+    public void unregisterIdlingResource(){
+        if(mIdlingResource != null){
+            IdlingRegistry.getInstance().unregister(mIdlingResource);
+        }
     }
 }
